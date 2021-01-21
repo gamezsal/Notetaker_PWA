4e7e4073284768a9a2f3bb2bd355d49784c84549
Sal Gamez <gamezsalvador@gmail.com>
2021-01-20 22:21:01 -0700

    added fetch in service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -18,6 +18,44 @@ const FILES_TO_CACHE = [
   '/assets/images/icons/icon-192x192.png',
   '/assets/images/icons/icon-384x384.png',
   '/assets/images/icons/icon-512x512.png',
+  '/assets/images/1.jpg',
+  '/assets/images/2.jpg',
+  '/assets/images/3.jpg',
+  '/assets/images/4.jpg',
+  '/assets/images/5.jpg',
+  '/assets/images/6.jpg',
+  '/assets/images/7.jpg',
+  '/assets/images/8.jpg',
+  '/assets/images/9.jpg',
+  '/assets/images/10.jpg',
+  '/assets/images/11.jpg',
+  '/assets/images/12.jpg',
+  '/assets/images/13.jpg',
+  '/assets/images/14.jpg',
+  '/assets/images/15.jpg',
+  '/assets/images/16.jpg',
+  '/assets/images/17.jpg',
+  '/assets/images/18.jpg',
+  '/assets/images/19.jpg',
+  '/assets/images/20.jpg',
+  '/assets/images/21.jpg',
+  '/assets/images/22.jpg',
+  '/assets/images/23.jpg',
+  '/assets/images/24.jpg',
+  '/assets/images/25.jpg',
+  '/assets/images/26.jpg',
+  '/assets/images/27.jpg',
+  '/assets/images/28.jpg',
+  '/assets/images/29.jpg',
+  '/assets/images/30.jpg',
+  '/assets/images/31.jpg',
+  '/assets/images/32.jpg',
+  '/assets/images/33.jpg',
+  '/assets/images/34.jpg',
+  '/assets/images/35.jpg',
+  '/assets/images/36.jpg',
+  '/assets/images/37.jpg',
+  '/assets/images/38.jpg'
 ];
 
 // install
@@ -54,36 +92,13 @@ self.addEventListener("install", function (evt) {
     self.clients.claim();
   });
 
-// fetch
-self.addEventListener("fetch", function(evt) {
-  // cache successful requests to the API
-  if (evt.request.url.includes("/api/")) {
+  self.addEventListener('fetch', function(evt) {
+    // code to handle requests goes here
     evt.respondWith(
-      caches.open(DATA_CACHE_NAME).then(cache => {
-        return fetch(evt.request)
-          .then(response => {
-            // If the response was good, clone it and store it in the cache.
-            if (response.status === 200) {
-              cache.put(evt.request.url, response.clone());
-            }
-
-            return response;
-          })
-          .catch(err => {
-            // Network request failed, try to get it from the cache.
-            return cache.match(evt.request);
+        caches.open(CACHE_NAME).then(cache => {
+          return cache.match(evt.request).then(response => {
+            return response || fetch(evt.request);
           });
-      }).catch(err => console.log(err))
-    );
-
-    return;
-  }
-
-  // if the request is not for the API, serve static assets using "offline-first" approach.
-  // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
-  evt.respondWith(
-    caches.match(evt.request).then(function(response) {
-      return response || fetch(evt.request);
-    })
-  );
-});
+        })
+      );
+    });
